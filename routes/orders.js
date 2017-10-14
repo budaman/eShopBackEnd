cb81c8b787a35db6348c4590a9a6049c6bc7cb91
budaman <budamantvydas@gmail.com>
2017-10-14 15:25:25 +0300

    ordering is working with

--- routes/orders.js
+++ routes/orders.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/order')
+const Product = require('../models/product')
+const User = require('../models/user')
 
 
 router.post('/createOrder', (req, res, next) => {
+  let quantity = req.body.product.quantity - req.body.quantity
+  let money = req.body.user.money - req.body.product.price
 
-  let newOrder = new Order({
-    user: req.body.user._id,
-    product: req.body.product._id,
-    quantity: req.body.quantity,
-    createdOn: req.body.createdOn
-  });
-
-  Order.addOrder(newOrder, (err, order) => {
-    if(err){
-      res.json({success: false, msg:'Failed to create order', err});
-    } else {
-      res.json(order);
-    }
-  });
+  if(quantity < 0) {
+    res.json({quantityProblem: true})
+  } else if (money < 0) {
+    res.json({moneyProblem: true})
+  } else {
+    let newOrder = new Order({
+      user: req.body.user._id,
+      product: req.body.product._id,
+      quantity: req.body.quantity,
+      createdOn: req.body.createdOn
+    });
+    Order.addOrder(newOrder, (err, order) => {
+      if(err){
+        res.json({success: false, msg:'Failed to create order', err});
+      } else {
+        res.json(order);
+      }
+    });
+    updProduct = req.body.product
+    updProduct.quantity = quantity
+    updUser = req.body.user
+    updUser.money = money
+    Product.findByIdAndUpdate(req.body.product._id, updProduct, function (err, post) {
+      if (err) return next(err);
+    });
+    User.findByIdAndUpdate(req.body.user._id, updUser, function (err, post) {
+      if (err) return next(err);
+    });
+  }
 });
 
 
